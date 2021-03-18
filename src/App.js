01f442337cba76ad2b913d585b69c435e93f8974
Nathaniel Shirts <nshirts7@gmail.com>
2021-03-17 23:34:07 -0600

    added a button that shows the first 20 pokemon, used api get.  Need to figure out a better way to sort the info.  Making call then sorting information

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pokemon: [],
+      show: false,
+    };
+  }
+
+  componentDidMount = () => {
+    axios
+      .get('https://pokeapi.co/api/v2/pokemon')
+      .then((res) => {
+        res.data.results.forEach((val) => {
+          axios
+            .get(val.url)
+            .then((res) => {
+              let oldState = this.state.pokemon;
+              this.setState({
+                pokemon: [
+                  ...oldState,
+                  {
+                    name: val.name,
+                    sprite: res.data.sprites.front_default,
+                    id: res.data.id,
+                  },
+                ],
+              });
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  handleShowPokemon = (params) => {
+    let list = this.state.pokemon.sort((a, b) => a.id - b.id);
+    this.setState({
+      pokemon: list,
+      show: !this.state.show,
+    });
+  };
+
+  render() {
+    // console.log(this.state);
+
+    const pokemonList = this.state.pokemon.map((val) => {
+      return (
+        <li key={val.sprite}>
+          Name : {val.name} sprite:{' '}
+          <img src={val.sprite} alt="pokemon sprite" /> ID : {val.id}
+        </li>
+      );
+    });
+
+    return (
+      <div>
+        <button onClick={this.handleShowPokemon}>Show Pokemon</button>
+        <ol>{this.state.show ? pokemonList : null}</ol>
+      </div>
+    );
+  }
 }
-
-export default App;
